38add2015197ee2f0de3d9aed3db6ff20bca037c
Noah Day <noah.day@adelaide.edu.au>
2023-04-03 13:22:08 +0100

    updating jupyter

--- untitled.m
+++ untitled.m
@@ -95,11 +95,82 @@ w = worldmap('world');
         geoshow(w, land, 'FaceColor', [0.5 0.5 0.5],'FaceAlpha',.5)
         colorbar; %cmocean('deep');
 %%
-pathname = '/Users/noahday/Gadi/ia40/waves-025/kmeans_2023-03-30.nc';
+pathname = '/Users/noahday/Gadi/ia40/kmeans_2009.nc';
 ncdisp(pathname)
 kmeans_data = ncread(pathname,'k');
-lon_k = ncread(pathname,'TLON');
+lon_k = ncread(pathname,'LON');
+lat_k = ncread(pathname,'LAT');
 size(kmeans_data)
+size(kmeans_data)
+
+close all
+figure
+w = worldmap('world');
+        axesm eqaazim; %, eqaazim eqdazim vperspec, eqdazim flips the x-axis, and y-axis to eqaazim. cassini
+        setm(w, 'Origin', [-90 0 0]);
+        setm(w, 'maplatlimit', [-90,-50]);
+        setm(w, 'grid', 'off');
+        setm(w, 'frame', 'off');
+        setm(w, "FontColor",[0.5, 0.5, 0.5])
+        setm(w, 'labelrotation', 'on')
+        %setm(w, 'meridianlabel', 'on','FontSize',font_size)
+        %setm(w, 'parallellabel', 'off','FontSize',font_size)
+        setm(w, 'mlabellocation', 60);
+        setm(w, 'plabellocation', 5);
+        pcolorm(lat_k,lon_k,kmeans_data(:,:,180),'FaceAlpha',0.99)
+        land = shaperead('landareas', 'UseGeoCoords', true);
+        geoshow(w, land, 'FaceColor', [0.5 0.5 0.5],'FaceAlpha',.5)
+        colorbar; %cmocean('deep');
+%%
+addpath functions
+addpath /Users/noahday/Github/cice-plotting-tools/functions
+addpath /Users/noahday/GitHub/CICE-analyser/processing
+kmeans_data = ncread("/Users/noahday/Gadi/ia40/kmean_2009.nc","k");
+for i = 1:365
+   miz_def = 0;
+  [MIZ_width(i,:)] = miz_width(kmeans_data(:,:,i),miz_def);
+end
+%%
+size(MIZ_width)
+
+close all
+figure
+
+plot(1:365, mean(MIZ_width'))
+%%
+filename1 = '/Users/noahday/Gadi/ia40/waves-10/iceh.2005-09-01.nc';
+filename2 = '/Volumes/NoahDay5TB/WIM_on/history/fullyears/iceh.2015-09-01.nc';
+
+lat1 = ncread(filename1,"TLAT");
+lat2 = ncread(filename2,"TLAT");
+
+lon1 = ncread(filename1,"TLON");
+lon2 = ncread(filename2,"TLON");
+
+fsdrad1 = ncread(filename1,"fsdrad");
+fsdrad2 = ncread(filename2,"fsdrad");
+
+close all
+figure
+w = worldmap('world');
+        axesm eqaazim; %, eqaazim eqdazim vperspec, eqdazim flips the x-axis, and y-axis to eqaazim. cassini
+        setm(w, 'Origin', [-90 0 0]);
+        setm(w, 'maplatlimit', [-90,-50]);
+        setm(w, 'grid', 'off');
+        setm(w, 'frame', 'off');
+        setm(w, "FontColor",[0.5, 0.5, 0.5])
+        setm(w, 'labelrotation', 'on')
+        %setm(w, 'meridianlabel', 'on','FontSize',font_size)
+        %setm(w, 'parallellabel', 'off','FontSize',font_size)
+        setm(w, 'mlabellocation', 60);
+        setm(w, 'plabellocation', 5);
+        pcolorm(lat1,lon1,fsdrad1-fsdrad2,'FaceAlpha',0.99)
+        land = shaperead('landareas', 'UseGeoCoords', true);
+        geoshow(w, land, 'FaceColor', [0.5 0.5 0.5],'FaceAlpha',.5)
+        colorbar; %cmocean('deep');
+%%
+
+
  %%
 x = [12 24 36];
 cpu_time = [1.66 1.5 2.2];
@@ -124,3 +195,61 @@ xlabel("Number of CPUs")
 exportgraphics(f,'walltime.pdf','ContentType','vector')
 
 
+
+function [MIZ_width] = miz_width(k_means,miz_def)
+
+% Loop around the south pole, each i is a line of constant longitude
+[len, wid, ~] = size(k_means);
+    %for i = 1:len
+    %    temp = k_means(i,~idx_miz);
+    %    [len1,wid1] = size(temp);
+    %    if len1 == 0
+    %        edge_class(i) = 0;
+    %    elseif wid1 == 0
+    %        edge_class(i) = 0;
+    %    else
+            % Take the last cell that > 0.15 SIC
+    %        edge_class(i) = temp(end); 
+    %    end
+    %end
+
+    %idx = isnan(edge_class);
+    %class = mode(edge_class(~idx));
+    MIZ = k_means == miz_def;%class;  
+    clear MIZ_width
+    [LT,LN,~,ulat,ulon] = grid_read('om2');
+    for i = 1:len
+        ulat_vec_temp = ulat(i,:);
+        ulon_vec_temp = ulon(i,:);
+        MIZ_vec_temp = MIZ(i,:);
+        if sum(MIZ_vec_temp) == 0
+            MIZ_width(i,1:2) = 0;
+        else
+            %south_point = find(MIZ_vec_temp, 1, 'first');
+            %north_point = find(MIZ_vec_temp, 1, 'last');
+            f = find(diff([0,MIZ_vec_temp,0]==1));
+            p = f(1:2:end-1);  % Start indices
+            y = f(2:2:end)-p;  % Consecutive ones counts
+            max_y = find(y == max(y));
+            p = p(max_y);
+            y = y(max_y);
+            south_point = p;
+            north_point = p+y;
+            % Use u-grid as it is positioned in the north east corner of
+            % the cell
+            MIZ_distance = pathdist([ulat_vec_temp(south_point-1),ulat_vec_temp(north_point)],[ulon_vec_temp(south_point),ulon_vec_temp(north_point)],'km');
+            MIZ_width(i,1:2) = MIZ_distance(1:2);
+
+            %for j = south_point:north_point
+            %    % Calculate the histogram P_z dz.
+            %    temp(j,1:2) = aice(i,j).*pathdist([ulat_vec_temp(j-1),ulat_vec_temp(j)],[ulon_vec_temp(j-1),ulon_vec_temp(j)],'km');
+            %end
+            % Integrate over, x_e = \int_0 ^x P_z dz.
+            %MIZ_width_corrected(i,1) = sum(temp(:,2));
+
+        end
+    end
+    MIZ_width = MIZ_width(:,2);
+    %MIZ_width = MIZ_width_corrected;
+
+end
